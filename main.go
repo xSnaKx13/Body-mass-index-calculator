09865830e84d35f8ddf218fddc736a4e2b8bc5e7
xsnakx13 <gametab20@gmail.com>
2025-08-01 11:13:08 +0300

    fixed restart bug

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	for {
 		var weight, height float64
-
+		fmt.Println("\n__- Калькулятор ИМТ-__\n")
 		err := userInput(&weight, &height)
 		if err != nil {
 			fmt.Print(err)
@@ -14,14 +14,17 @@ func main() {
 		textResult := result(IMTResult)
 		fmt.Printf("Ваш ИМТ: %.2f: %s", IMTResult, textResult)
 
-		for {
+		isWork := true
+		for isWork {
 			fmt.Print("1 - расчитать повторно\n2 - выход\n")
 			var input int
 			fmt.Scan(&input)
 
 			switch input {
 			case 1:
-				continue
+				isWork = false
+				var temp string
+				fmt.Scanln(&temp)
 			case 2:
 				return
 			default:
